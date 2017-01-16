e2df97a33de58c8969f93ce388cf3130a7bd7caf
Tero Marttila <tero.marttila@kontena.io>
2017-01-16 18:23:00 +0200

    nginx-lb: register http

--- registrator/policies/nginx-lb.rb
+++ registrator/policies/nginx-lb.rb
@@ -4,6 +4,23 @@ def docker_container(container)
 
   ipv4 = overlay_cidr.split('/')[0]
 
+  if port = container.env['io.kontena.nginx.http']
+    aliases = container.env['io.kontena.nginx.http.aliases']
+    aliases = aliases.split if aliases
+
+    config = {
+      'port'     => container.env['io.kontena.nginx.http.port'],
+      'host'     => container.env['io.kontena.nginx.http.host'],
+      'aliases'  => aliases,
+      'location' => container.env['io.kontena.nginx.http.location'],
+    }.reject{|key, value| value.nil?}
+
+    yield ({
+      "/kontena/nginx/http/#{service}/server" => config,
+      "/kontena/nginx/http/#{service}/upstreams/#{container.hostname}" => { ipv4: ipv4, port: port },
+    })
+  end
+
   if port = container.env['io.kontena.nginx.tcp']
     yield ({
       "/kontena/nginx/tcp/#{service}/server" => { port: port },
